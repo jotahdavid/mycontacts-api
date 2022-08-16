0a1710897b210744a3cb1f0165f70b04ff1d9fed
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-08-16 17:05:17 -0300

    feat: handle request errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -6,5 +7,13 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, req, res, next) => {
+  if (error.routine === 'string_to_uuid') {
+    console.error(error);
+    return res.status(404).json({ error: 'Invalid ID syntax for type uuid' });
+  }
+  console.error(error);
+  return res.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server running at http://localhost:3000'));
