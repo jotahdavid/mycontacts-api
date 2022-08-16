ef9f2215449ab5412c47f56ef08c2a0c7bf18b02
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-08-16 15:30:44 -0300

    feat: add relationship between contact and category

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -1,23 +1,30 @@
 const db = require('../../database');
 
+const TABLE_NAME = 'contacts';
+
 /**
  * @typedef {{
  *  id: string;
  *  name: string;
- *  email: string;
- *  phone: string;
- *  category_id: string;
+ *  email: string | null;
+ *  phone: string | null;
+ *  category_id: string | null;
  * }} Contact
  */
 
 class ContactRepository {
   /**
    * @param {string} orderQuery
-   * @returns {Promise<Contact[]>}
+   * @returns {Promise<(Contact & { category_name: string | null; })[]>}
    */
   async findAll(orderQuery = 'ASC') {
     const direction = orderQuery.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
-    const rows = await db.query(`SELECT * FROM contacts ORDER BY name ${direction}`);
+    const rows = await db.query(`
+      SELECT contacts.*, categories.name AS category_name
+      FROM ${TABLE_NAME}
+      LEFT JOIN categories ON categories.id = contacts.category_id
+      ORDER BY contacts.name ${direction}
+    `);
     return rows;
   }
 
@@ -26,7 +33,12 @@ class ContactRepository {
    * @returns {Promise<Contact | undefined>}
    */
   async findById(id) {
-    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    const [row] = await db.query(`
+      SELECT contacts.*, categories.name AS category_name
+      FROM ${TABLE_NAME}
+      LEFT JOIN categories ON categories.id = contacts.category_id
+      WHERE contacts.id = $1
+    `, [id]);
     return row;
   }
 
@@ -35,7 +47,12 @@ class ContactRepository {
    * @returns {Promise<Contact | undefined>}
    */
   async findByEmail(email) {
-    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
+    const [row] = await db.query(`
+      SELECT contacts.*, categories.name AS category_name
+      FROM ${TABLE_NAME}
+      LEFT JOIN categories ON categories.id = contacts.category_id
+      WHERE contacts.email = $1
+    `, [email]);
     return row;
   }
 
@@ -47,7 +64,7 @@ class ContactRepository {
     name, email, phone, category_id,
   }) {
     const [row] = await db.query(`
-      INSERT INTO contacts(name, email, phone, category_id)
+      INSERT INTO ${TABLE_NAME}(name, email, phone, category_id)
       VALUES($1, $2, $3, $4)
       RETURNING *
     `, [name, email, phone, category_id]);
@@ -57,13 +74,13 @@ class ContactRepository {
   /**
    * @param {string} id
    * @param {Omit<Contact, "id">} contact
-   * @returns {Contact}
+   * @returns {Promise<Contact>}
    */
   async update(id, {
     name, email, phone, category_id,
   }) {
     const [row] = await db.query(`
-      UPDATE contacts
+      UPDATE ${TABLE_NAME}
       SET name = $1, email = $2, phone = $3, category_id = $4
       WHERE id = $5
       RETURNING *
@@ -75,8 +92,7 @@ class ContactRepository {
    * @param {string} id
    */
   async delete(id) {
-    const [row] = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
-    return row;
+    await db.query(`DELETE FROM ${TABLE_NAME} WHERE id = $1`, [id]);
   }
 }
 
