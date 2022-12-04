7bde39ac3be28d931c40e21a39c1f6cbbcde20f3
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-12-04 15:34:42 -0300

    feat: can set cors origins by .env variable

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,5 +1,8 @@
 const cors = require('cors');
 
+const { ALLOWED_ORIGINS } = process.env;
+const origin = ALLOWED_ORIGINS ? ALLOWED_ORIGINS.split(';') : [];
+
 module.exports = cors({
-  origin: ['http://localhost:3000'],
+  origin,
 });
